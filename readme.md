d1be2a04b4dce115c4713b911e6e4d8cfdab0609
yaminitailor <yaminitailor.mobiiworld@gmail.com>
2021-08-26 12:53:26 +0530

    crud operation perfom

---
+++ readme.md
@@ -0,0 +1,27 @@
+## **Laravel School Management System** 
+
+<hr />  
+
+There are 3 types of user accounts. They include:
+ 
+Administrators (Super Admin & Admin)
+- Teacher
+- Student
+- Admin
+
+
+**Installation**
+- Install dependencies (composer install)
+- Set Database Credentials & App Settings in dotenv file (.env)
+- Migrate Database (php artisan migrate)
+- Database seed (php artisan db:seed)
+
+**Login Credentials**
+After seeding. Login details as follows:
+
+| Account Type  | Username | Email | Password |
+| ------------- | -------- | ----- | -------- |
+| Super Admin | sa | superadmin@gmail.com | 123456 |
+|  Admin | admin | admin@admin.com | 123456 |
+|  Teacher | teacher | teacher@teacher.com | 123456 |
+|  Student | student | student@student.com | 123456 |
---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+Auth::routes();
+
+//Route::get('/test', 'TestController@index')->name('test');
+
+
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('/', 'HomeController@dashboard')->name('home');
+    Route::get('/home', 'HomeController@dashboard')->name('home');
+    Route::get('/dashboard', 'HomeController@dashboard')->name('dashboard');
+
+  
+    /*************** Support Team *****************/
+    Route::group(['namespace' => 'SupportTeam',], function(){
+
+        /*************** Students *****************/
+        Route::group(['prefix' => 'students'], function(){
+            Route::get('reset_pass/{st_id}', 'StudentRecordController@reset_pass')->name('st.reset_pass');
+            Route::get('list/{class_id}', 'StudentRecordController@listByClass')->name('students.list');
+
+         
+        });
+
+        /*************** Users *****************/
+        Route::group(['prefix' => 'users'], function(){
+            Route::get('reset_pass/{id}', 'UserController@reset_pass')->name('users.reset_pass');
+        });
+
+
+        /*************** Payments *****************/
+        Route::group(['prefix' => 'payments'], function(){
+
+            Route::get('manage/{class_id?}', 'PaymentController@manage')->name('payments.manage');
+            Route::get('invoice/{id}/{year?}', 'PaymentController@invoice')->name('payments.invoice');
+            Route::get('receipts/{id}', 'PaymentController@receipts')->name('payments.receipts');
+            Route::get('pdf_receipts/{id}', 'PaymentController@pdf_receipts')->name('payments.pdf_receipts');
+            Route::post('select_year', 'PaymentController@select_year')->name('payments.select_year');
+            Route::post('select_class', 'PaymentController@select_class')->name('payments.select_class');
+            Route::delete('reset_record/{id}', 'PaymentController@reset_record')->name('payments.reset_record');
+            Route::post('pay_now/{id}', 'PaymentController@pay_now')->name('payments.pay_now');
+        });
+
+
+
+        Route::resource('students', 'StudentRecordController');
+        Route::resource('users', 'UserController');
+        Route::resource('classes', 'MyClassController');
+        Route::resource('sections', 'SectionController');
+        Route::resource('payments', 'PaymentController');
+
+
+
+    /************************ AJAX ****************************/
+    Route::group(['prefix' => 'ajax'], function() {
+        Route::get('get_class_sections/{class_id}', 'AjaxController@get_class_sections')->name('get_class_sections');
+    });
+
+});
+
+
